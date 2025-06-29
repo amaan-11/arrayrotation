c550ad9592135a5a7bca5825748dcf4eda4cf347
Amaan <amaan.brs@gmail.com>
2025-06-29 22:05:13 +0300

    Learning array rotation

---
+++ arrayrotation.c
@@ -0,0 +1,50 @@
+//
+// Created by Amaan on 6/29/2025.
+//
+
+
+#include <stdio.h>
+
+void reverse(int* arr, int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start++] = arr[end];
+        arr[end--] = temp;
+    }
+}
+
+void rotateArr(int* arr, int n, int d) {
+
+    d %= n;
+
+    reverse(arr, 0, d - 1);
+
+    reverse(arr, d, n - 1);
+
+    reverse(arr, 0, n - 1);
+}
+
+int main() {
+    printf("Enter how many elements are gonna be in the array:");
+    int n;
+    scanf("%d", &n);
+    printf("Array will be from 1 to %d\n", n);
+    while (n <= 0) {
+        printf("Please enter a valid number of elements: ");
+        scanf("%d", &n);
+    }
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+    }
+    int d;
+    printf("Enter the number of positions by which to rotate the array: ");
+    scanf("%d", &d);
+
+
+    rotateArr(arr, n, d);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
